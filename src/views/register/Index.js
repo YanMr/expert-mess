aadfd91350bb567883337118a5897f0534acc34f
zhuk15 <zhuk15@chinaunicom.cn>
2021-12-19 16:59:06 +0800

    注册页

--- src/views/register/Index.js
+++ src/views/register/Index.js
@@ -1,5 +1,197 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Button, Form, Input, message } from 'antd';
+import top from '../../assets/img/login_banner.jpeg';
+import zhuanjia from '../../assets/img/zhuanjia.png';
+import './index.less';
 
-const Register = () => <div className="shadow-radius">注册</div>;
+const FormItem = Form.Item;
 
-export default Register;
+const nameRules = { required: true, message: "请输入姓名" };
+const passwordRules = { required: true, message: "请输入密码" };
+const surepasswordRules = { required: true, message: "请输入正确密码" };
+const phoneRules = { required: true, message: "请输入手机号" };
+const yzmRules = { required: true, message: "请输入验证码" };
+class Register extends Component {
+  formRef = React.createRef();
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      btnBool: false,
+      btnText: '获取验证码',
+      buttonCode: 'ButtonDefault',
+    }
+  }
+  componentDidMount() {
+
+  }
+
+  onRChange = (e, txt) => {
+    switch (txt) {
+      case 'email':
+        this.setState({ email: e.target.value })
+        break;
+      default:
+        break;
+    }
+  }
+
+  surePassword() {
+    const values = this.formRef.current.getFieldsValue()
+    if (values.password !== values.surepassword) {
+      message.error("两次输入的密码不相同，请重新输入！！！")
+    }
+  }
+
+  sendVerCode() {
+    let maxTime = 30;
+
+    this.timer = setInterval(() => {
+      if (maxTime > 0) {
+        --maxTime;
+        this.setState({
+          btnText: '已发送' + maxTime,
+          buttonCode: 'buttonCode',
+          btnBool: true
+        })
+      } else {
+        clearInterval(this.timer);
+        this.setState({
+          btnText: '获取验证码',
+          buttonCode: 'ButtonDefault',
+          btnBool: false
+        })
+      }
+    }, 1000);
+
+    // axiosRequest(`/platform_portal/api/forget/mail?email=${this.state.email}`, 'GET').then(res => {
+    //     if (res.code == 200) {
+    //       message.info(res.data)
+    //     } else {
+    //       message.error(res.message);
+    //       clearInterval(this.timer);
+    //       this.setState({
+    //         btnText: '获取验证码',
+    //         buttonCode:'ButtonDefault',
+    //         btnBool: false
+    //       })
+    //     }
+    //     this.setState({
+    //       confirmloading:false
+    //     })
+    // })
+  }
+
+  onFinish = (values) => {
+    if (values.password !== values.surepassword) {
+      message.error("两次输入的密码不相同，请重新输入！！！")
+      return
+    } else {
+      console.log('Success:', values);
+      console.log('formRef:', this.formRef);
+    }
+  };
+
+  onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  render() {
+    const { btnBool, btnText } = this.state
+    const renderEmailAndPsw = (
+      <div>
+        <Form
+          name="basic"
+          ref={this.formRef}
+          labelCol={{ span: 5 }}
+          wrapperCol={{ span: 12 }}
+          initialValues={{ remember: true }}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}
+          autoComplete="off"
+        >
+
+          <FormItem label="用户名" name="username" rules={[nameRules]}>
+            <Input className="input" placeholder="请输入用户名" />
+          </FormItem>
+
+          <FormItem label="密码" name="password" rules={[passwordRules]}>
+            <Input className="input" placeholder="请输入用户名" />
+          </FormItem>
+
+          <FormItem label="确认密码" name="surepassword" rules={[surepasswordRules]}>
+            <Input className="input" placeholder="请再次输入密码" onBlur={this.surePassword.bind(this)} />
+          </FormItem>
+
+          <FormItem label="手机号" name="phonenumber" rules={[phoneRules]}>
+            <Input className="input" placeholder="请再次输入手机号码" />
+          </FormItem>
+
+          <div className="yanzhengma">
+            <FormItem label="验证码" name="yanzhengma" rules={[yzmRules]}>
+              <Input
+                allowClear
+                className="inputverify"
+                onChange={(e) => this.onRChange(e, 'emailrt')}
+                placeholder="请输入验证码"
+              />
+            </FormItem>
+
+            <div className="getcode">
+              <FormItem>
+                <Button className={btnBool ? 'buttonCode' : 'ButtonDefault'}
+                  disabled={btnBool}
+                  onClick={this.sendVerCode.bind(this)}
+                >{btnText}</Button>
+              </FormItem>
+            </div>
+          </div>
+
+          <div className="resetsumit">
+            <div className="btn">
+              <FormItem>
+                <Button type="default" size="large" htmlType="reset">
+                  重置
+                </Button>
+              </FormItem>
+            </div>
+
+            <div className="btn">
+              <FormItem>
+                <Button type="primary" size="large" htmlType="submit">
+                  注册
+                </Button>
+              </FormItem>
+            </div>
+          </div>
+        </Form>
+      </div>
+    )
+
+    return (
+      <div>
+        <div className="box">
+          <div className="leftbox">
+            <img className="logo"
+              src={zhuanjia} alt=''
+            />
+            <div className="zhezhao"></div>
+            <div className="leftimg">
+              <img className="logo2"
+                src={top} alt=''
+              />
+            </div>
+          </div>
+          <div className="rightboxre">
+            <div className="stepnew"></div>
+            <div className="formstep">
+              {renderEmailAndPsw}
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Register
